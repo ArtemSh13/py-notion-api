48b954c5e20f46b23220fdbc7b6f435f69a335b5
ArtemSh13 <mozg1303@ya.ru>
2024-03-11 13:42:36 +0300

    Add retrieve_database method to NotionDB class

--- notion-api/notion_db.py
+++ notion-api/notion_db.py
@@ -1,4 +1,15 @@
+import requests
+
+
 class NotionDB:
-    def __init__(self, bearer_token, db_id):
+    def __init__(self, bearer_token, database_id):
+        self.NOTION_VERSION = "2022-02-22"
+        self.NOTION_ENDPOINT_DB = "https://api.notion.com/v1/databases"
         self.bearer_token = bearer_token
-        self.db_id = db_id
+        self.database_id = database_id
+
+    def retrieve_database(self):
+        headers = {"Authorization": f"Bearer {self.bearer_token}",
+                   "Notion-Version": self.NOTION_VERSION}
+        response = requests.get(f"{self.NOTION_ENDPOINT_DB}/{self.database_id}",
+                                headers=headers)
